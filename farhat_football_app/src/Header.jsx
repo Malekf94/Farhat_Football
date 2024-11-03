866a79f0f1de9c5336c77f2e53f7e623d9a94549
Malekf94 <malek.f.94@gmail.com>
2024-11-03 23:37:05 +0000

    Recreated react app

---
+++ farhat_football_app/src/Header.jsx
@@ -0,0 +1,30 @@
+function Header() {
+	return (
+		<header>
+			<h1>My website</h1>
+			<nav>
+				<a href="index.html">
+					<img src="images/Farhatfootballlogo1.jpeg" />
+				</a>
+				<div>
+					<ul>
+						<li>
+							<a href="./rules.html">Rules</a>
+						</li>
+						<li>
+							<a href="">Matches</a>
+						</li>
+						<li>
+							<a href="">Players</a>
+						</li>
+						<li>
+							<a href="">Your Account</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	);
+}
+
+export default Header;
